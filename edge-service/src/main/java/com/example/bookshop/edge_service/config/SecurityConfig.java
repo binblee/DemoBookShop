b96851660beccac6bd252d37da5df64ab3dedcd1
Li Bin <binblee@users.noreply.github.com>
2024-12-26 18:37:04 +0800

    ui and csrf token

--- edge-service/src/main/java/com/example/bookshop/edge_service/config/SecurityConfig.java
+++ edge-service/src/main/java/com/example/bookshop/edge_service/config/SecurityConfig.java
@@ -1,16 +1,25 @@
 package com.example.bookshop.edge_service.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
-import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.security.web.server.csrf.CsrfToken;
+import org.springframework.security.web.server.csrf.XorServerCsrfTokenRequestAttributeHandler;
+import org.springframework.web.server.WebFilter;
 
-// @Configuration(proxyBeanMethods = false)
-@EnableWebFlux
+import reactor.core.publisher.Mono;
+
+@Configuration(proxyBeanMethods = false)
+// @EnableWebFlux
 public class SecurityConfig {
     @Bean
     SecurityWebFilterChain springSecurityFilterChain(
@@ -18,9 +27,20 @@ public class SecurityConfig {
         ReactiveClientRegistrationRepository clientRegistrationRepository
         ) {
         return http
-                .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+                .authorizeExchange(exchange -> exchange
+                    .pathMatchers("/", "/*.css", "/*.js", "/favicon.ico").permitAll()
+                    .pathMatchers(HttpMethod.GET, "/books/**").permitAll()
+                    .anyExchange().authenticated()
+                )
+                .exceptionHandling(exceptionHandling ->
+                    exceptionHandling.authenticationEntryPoint(
+                        new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)
+                    ))
                 .oauth2Login(Customizer.withDefaults())
                 .logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepository)))
+                .csrf(csrf -> csrf
+                    .csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse())
+                    .csrfTokenRequestHandler(new XorServerCsrfTokenRequestAttributeHandler()::handle))
                 .build();
     }
 
@@ -30,4 +50,16 @@ public class SecurityConfig {
             oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
             return oidcLogoutSuccessHandler;
     }
+
+	@Bean
+	WebFilter csrfWebFilter() {
+		// Required because of https://github.com/spring-projects/spring-security/issues/5766
+		return (exchange, chain) -> {
+			exchange.getResponse().beforeCommit(() -> Mono.defer(() -> {
+				Mono<CsrfToken> csrfToken = exchange.getAttribute(CsrfToken.class.getName());
+				return csrfToken != null ? csrfToken.then() : Mono.empty();
+			}));
+			return chain.filter(exchange);
+		};
+	}
 }
